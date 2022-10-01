1c9db20570729bdaa9daf00a23fc7b346bd6b32b
bontaramsonta <18cs113@gmail.com>
2022-10-01 11:45:53 +0530

    mongoplay dump

--- apps/fastify/extras/archive.md
+++ apps/fastify/extras/archive.md
@@ -1,1 +1,31 @@
 # Archive
+
+## [**mongo**] category redirectUrl Fixer
+```js
+  function redirectUrlGenerator(s) {
+    return s.trim().replaceAll(/[<>()'"%!$#@*^.?:;|\\+_=*]/g, "").replaceAll(/[ ,.]/g, "-").replaceAll("&", "and").replaceAll(/--*/g, "-").toLowerCase()
+  }
+  console.log("[main]")
+  let redirectUrls = await client.db("marketplace").collection("productcategories").find({}, {
+    redirectUrl: 1,
+    title: 1
+  }).toArray()
+  //! for extra test cases
+  // redirectUrls.push(additional)
+  let re = []
+  await Promise.all(redirectUrls.map(async (cat, i) => {
+    if(!cat.isDeleted) {
+      let newRedirectUrl = redirectUrlGenerator(cat.redirectUrl)
+      let c = 0
+      re.forEach(r => r === newRedirectUrl ? c++ : null)
+      if (c !== 0) {
+        newRedirectUrl += c
+        console.log("[deDuped]",newRedirectUrl)
+      }
+      re.push(newRedirectUrl)
+      console.log(`[${cat.title}]`, cat.redirectUrl, "=>", newRedirectUrl)
+      return await client.db("marketplace").collection("productcategories").updateOne({ _id: cat._id }, { $set: { redirectUrl: newRedirectUrl } })
+    }
+  }))
+  console.log("[done]")
+```
--- apps/fastify/extras/mongoplay.js
+++ apps/fastify/extras/mongoplay.js
@@ -5,11 +5,10 @@
 import { MongoClient } from "mongodb"
 
 // #region BOOTSTRAP
-const client = new MongoClient("mongodb://localhost:27018", {
+const client = new MongoClient("mongodb://localhost:27017", {
   appName: "migrations",
   keepAlive: false,
   maxPoolSize: 1,
-  directConnection: true
 })
 
 client.once("open", async () => {
@@ -45,43 +44,6 @@ client.connect()
 
 export default async function main() {
   console.log("[main]")
-  client.db("global").collection("base").insertOne({
-    appList: ["cabigo", "getTaxi", "fleto"],
-    apps: {
-      cabigo: {
-        name: "cabigo",
-        remote: false,
-        url: "mongodb://localhost:27017/cabigo",
-        connOptions: {
-          maxPoolSize: 1,
-          directConnection: true
-        }
-      },
-      getTaxi: {
-        name: "getTaxi",
-        remote: false,
-        url: "mongodb://localhost:27017/getTaxi",
-        connOptions: {
-          maxPoolSize: 1,
-          directConnection: true
-        }
-      },
-      fleto: {
-        name: "fleto",
-        remote: false,
-        url: "mongodb://localhost:27017/fleto",
-        connOptions: {
-          maxPoolSize: 1,
-          directConnection: true
-        }
-      }
-    }
-  })
-  client.db("cabigo").collection("base").insertOne({
-    name: "cabigo",
-    fullName: "Cabigo Taxi",
-    services: ["local", "outstation", "rental"],
-    roles: ["admin", "sup-admin", "sub-admin", "driver", "user"],
-    users: ["user:get-own", "user:set-own", "booking:get-one"]
-  })
+
+  console.log("[done]")
 }
